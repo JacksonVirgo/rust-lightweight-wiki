350fc95113f89268c512ea7904ed35b2aa9c6221
Jack Nash <jacknash42@gmail.com>
2024-01-13 04:40:26 +1000

    fix: changed order of formatting md lists

--- src/utils/formatting.rs
+++ src/utils/formatting.rs
@@ -1,14 +1,9 @@
+
 pub fn parse_markdown(markdown_input: &str) -> String {
     let mut html_output = String::new();
     let mut in_list = false;
     for line in markdown_input.lines() {
-        if line.starts_with("# ") {
-            html_output.push_str(&format!("<h1>{}</h1>", &line[2..]));
-        } 
-        else if line.starts_with("## ") {
-            html_output.push_str(&format!("<h2>{}</h2>", &line[2..]));
-        }
-        else if line.starts_with("* ") {
+        if line.starts_with("* ") {
             if !in_list {
                 html_output.push_str("<ul>");
                 in_list = true;
@@ -19,6 +14,15 @@ pub fn parse_markdown(markdown_input: &str) -> String {
                 html_output.push_str("</ul>");
                 in_list = false;
             }
+        }
+
+        if line.starts_with("### ") {
+            html_output.push_str(&format!("<h3>{}</h3>", &line[4..]));
+        } else if line.starts_with("## ") {
+            html_output.push_str(&format!("<h2>{}</h2>", &line[3..]));
+        } else if line.starts_with("# ") {
+            html_output.push_str(&format!("<h1>{}</h1>", &line[2..]));
+        } else {
             html_output.push_str(&format!("<p>{}</p>", line));
         }
     }
